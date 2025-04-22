846b278a5aea4a8edfab8d108894c4c11fb0a01c
Ellie Q <qelliehuynh@gmail.com>
2025-04-21 23:01:03 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import { useState } from "react";
 import { puppyList } from "./data";
-
-
+import "./App.css";
+import "./index.css";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [count, setCount] = useState(0);
   console.log(puppyList);
-  const [featPupId, setFeatPupId] = useState(null)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log (featuredPup)
 
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p onClick={()=> {console.log ("puppy id: ", puppy.id )}} key={puppy.id}> {puppy.name} <br/> {puppy.age} </p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      <hr />
+  
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
     </div>
   );
 }

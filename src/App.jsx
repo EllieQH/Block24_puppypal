2765c98859518a253fe45e8e80733c6d0a8020e6
Ellie Q <qelliehuynh@gmail.com>
2025-04-21 22:35:23 -0500

    add onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { puppyList } from "./data";
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [count, setCount] = useState(0);
   console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p>{puppy.name}</p>;
+        return <p onClick={()=> {console.log ("puppy id: ", puppy.id )}} key={puppy.id}> {puppy.name} <br/> {puppy.age} </p>;
       })}
     </div>
   );
